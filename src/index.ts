9768905635fe468bd555fe9b1c09321a0b8ef7a4
abrarasif11 <faasif929@gmail.com>
2025-11-14 14:12:16 +0600

    create data by prisma

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  //   const result = await prisma.post.create({
+  //     data: {
+  //       title: "Updated Title",
+  //       content: "This is updated content for the data object.",
+  //       author: "Fahim Abrar Asif",
+  //     },
+  //   });
+  //   console.log(result);
+};
+main();
