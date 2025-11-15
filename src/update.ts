954bba6e8857bbc0445bf6fb4b5ec10cb15730ee
abrarasif11 <faasif929@gmail.com>
2025-11-15 10:56:59 +0600

    update and updateMany

---
+++ src/update.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const updates = async () => {
+  //   const singleUpdate = await prisma.post.update({
+  //     where: {
+  //       id: 6,
+  //     },
+  //     data: {
+  //       title: "updated data",
+  //       content: "updated content",
+  //       author: "Fahim Abrar Asif",
+  //     },
+  //   });
+
+  const updateMany = await prisma.post.updateMany({
+    where: {
+      title: "Mastering JavaScript",
+    },
+    data: {
+      published: true,
+    },
+  });
+  console.log("updateMany");
+};
+updates();
